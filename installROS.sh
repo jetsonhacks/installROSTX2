7f2c989975307123d2a21cf72b05bcb784f3b341
Jan Hadrava <had@kam.mff.cuni.cz>
2019-03-19 20:33:25 +0100

    Specify full key ID for apt-key

--- installROS.sh
+++ installROS.sh
@@ -87,7 +87,7 @@ sudo apt-add-repository restricted
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 # Installation
 tput setaf 2
 echo "Updating apt-get"
